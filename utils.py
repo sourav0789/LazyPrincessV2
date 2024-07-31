24b5571ad7a410f293cf710a1f178afb84b0fdc2
sourav0789 <souravbosu3487@gmail.com>
2024-07-31 15:27:46 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -377,28 +377,121 @@ def humanbytes(size):
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
-async def get_shortlink(link):
+async def extract_verified_short_link(link):
+    API = LAZY_SHORTNER_API if LAZY_SHORTNER_API else URL_SHORTNER_WEBSITE_API
+    URL = LAZY_SHORTNER_URL if LAZY_SHORTNER_URL else URL_SHORTENR_WEBSITE
     https = link.split(":")[0]
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
 
-    url = f'https://teraboxlinks.com/shortLink'
-    params = {'token': URL_SHORTNER_WEBSITE_API,
-              'link': link,
-              'format': 'json'
-              }
+    if URL == "api.shareus.in":
+        url = f"https://{URL}/shortLink"
+        params = {"token": API,
+                  "format": "json",
+                  "link": link,
+                  }
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                    data = await response.json(content_type="text/html")
+                    if data["status"] == "success":
+                        return data["shortlink"]
+                    else:
+                        logger.error(f"Error: {data['message']}")
+                        return f'https://{URL}/shortLink?token={API}&format=json&link={link}'
 
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                data = await response.json(content_type='text/html')
-                if data["status"] == "success":
-                    return data['shortlink']
-                else:
-                    logger.error(f"Error: {data['message']}")
-                    return f'https://teraboxlinks.com/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+        except Exception as e:
+            logger.error(e)
+            return f'https://{URL}/shortLink?token={API}&format=json&link={link}'
+    else:
+        url = f'https://{URL}/api'
+        params = {'api': API,
+                  'url': link,
+                  }
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                    data = await response.json()
+                    if data["status"] == "success":
+                        return data['shortenedUrl']
+                    else:
+                        logger.error(f"Error: {data['message']}")
+                        return f'https://{URL}/api?api={API}&link={link}'
 
-    except Exception as e:
-        logger.error(e)
-        return f'https://teraboxlinks.com/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+        except Exception as e:
+            logger.error(e)
+            print(e)
+            return f'{URL}/api?api={API}&link={link}'
+
+#feel free to share @LazyDeveloperr ❤️
+#join main channel @LazyDeveloper 🎉
+# paste this code at the bottom of the utils.py
+async def check_token(bot, userid, token):
+    user = await bot.get_users(userid)
+    if not await db.is_user_exist(user.id):
+        await db.add_user(user.id, user.first_name)
+        await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
+    if user.id in TOKENS.keys():
+        TKN = TOKENS[user.id]
+        if token in TKN.keys():
+            is_used = TKN[token]
+            if is_used == True:
+                return False
+            else:
+                return True
+    else:
+        return False
+
+#feel free to share @LazyDeveloperr ❤️
+#join main channel @LazyDeveloper 🎉
+# paste this code at the bottom of the utils.py
+async def get_token(bot, userid, link):
+    user = await bot.get_users(userid)
+    if not await db.is_user_exist(user.id):
+        await db.add_user(user.id, user.first_name)
+        await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
+    token = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
+    TOKENS[user.id] = {token: False}
+    link = f"{link}verify-{user.id}-{token}"
+    print(link)
+    final_verified_lazy_link = await extract_verified_short_link(link)
+    return str(final_verified_lazy_link)
+
+#feel free to share @LazyDeveloperr ❤️
+#join main channel @LazyDeveloper 🎉
+# paste this code at the bottom of the utils.py
+async def verify_user(bot, userid, token):
+    user = await bot.get_users(userid)
+    if not await db.is_user_exist(user.id):
+        await db.add_user(user.id, user.first_name)
+        await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
+    TOKENS[user.id] = {token: True}
+    tz = pytz.timezone('Asia/Kolkata')
+    today = date.today()
+    VERIFIED[user.id] = str(today)
+
+#feel free to share @LazyDeveloperr ❤️
+#join main channel @LazyDeveloper 🎉
+# paste this code at the bottom of the utils.py
+async def check_verification(bot, userid):
+    user = await bot.get_users(userid)
+    if not await db.is_user_exist(user.id):
+        await db.add_user(user.id, user.first_name)
+        await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
+    tz = pytz.timezone('Asia/Kolkata')
+    today = date.today()
+    if user.id in VERIFIED.keys():
+        EXP = VERIFIED[user.id]
+        years, month, day = EXP.split('-')
+        comp = date(int(years), int(month), int(day))
+        if comp<today:
+            return False
+        else:
+            return True
+    else:
+        return False
+    
+
+#feel free to share @LazyDeveloperr ❤️
+#join main channel @LazyDeveloper 🎉
